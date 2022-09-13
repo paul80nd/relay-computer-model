fb24c68afc8416f94fbbdf851e5516c70cb84657
Paul Law <paul@80nd.co.uk>
2022-09-13 20:00:22 +0100

    decoder tests

---
+++ test/cards/decoder.test.ts
@@ -0,0 +1,56 @@
+'use strict';
+
+import * as assert from 'assert';
+import { BitValue } from '../../src/bit_value';
+import { BusFactory } from '../../src/bus/bus';
+import { BusGroupFactory } from '../../src/bus/bus_groups';
+import { BusPartFactory } from '../../src/bus/bus_parts';
+import { OperationLines } from '../../src/bus/bus_part_lines';
+import { CardFactory } from '../../src/cards';
+import { CardPart } from '../../src/cards/card_part';
+
+suite('card-decoder', () => {
+
+  const bf = new BusFactory(new BusPartFactory());
+  const bgf = new BusGroupFactory(bf);
+  const cf = new CardFactory();
+
+  const card = cf.createDecoder();
+  const bgs = bgf.createBusGroups();
+  card.connect(bgs.w);
+
+  const cpip = new CardPart();
+  bgs.w.controlInstructionBus.instructionPart.connect(cpip);
+
+  const cpop = bgs.w.operationBus.operationPart;
+
+  test('goto', function () {
+    cpip.value = BitValue.fromUnsignedNumber(0b11000000);
+    assert.ok(cpop.value.bit(OperationLines.IGTO));
+  });
+
+  test('alu', function () {
+    cpip.value = BitValue.fromUnsignedNumber(0b10000000);
+    assert.ok(cpop.value.bit(OperationLines.IALU));
+  });
+
+  test('alu', function () {
+    cpip.value = BitValue.fromUnsignedNumber(0b01000000);
+    assert.ok(cpop.value.bit(OperationLines.ISET));
+  });
+
+  test('mov8', function () {
+    cpip.value = BitValue.fromUnsignedNumber(0b00000000);
+    assert.ok(cpop.value.bit(OperationLines.IMV8));
+  });
+
+  test('sequence', function () {
+    cpip.value = BitValue.fromUnsignedNumber(0b01000000);
+    cpip.value = BitValue.fromUnsignedNumber(0b00000000);
+    cpip.value = BitValue.fromUnsignedNumber(0b10000000);
+    assert.ok(!cpop.value.bit(OperationLines.ISET));
+    assert.ok(!cpop.value.bit(OperationLines.IMV8));
+    assert.ok(cpop.value.bit(OperationLines.IALU));
+  });
+
+});
