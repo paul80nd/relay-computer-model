543043c451231147a91da9c2509c2695f95f3362
Paul Law <paul@80nd.co.uk>
2022-09-14 18:26:59 +0100

    test reg m xy

--- test/cards/register_xy.test.ts
+++ test/cards/register_xy.test.ts
@@ -19,12 +19,15 @@ suite('card-register-xy', () => {
   const bgs = bgf.createBusGroups();
   card.connect(bgs.y);
 
+  const addrIn = new CardPart();
   const dataIn = new CardPart();
   const ctrlIn = new CardPart();
+  bgs.y.addressBus.addressPart.connect(addrIn);
   bgs.y.dataControlBus.dataPart.connect(dataIn);
   bgs.y.controlYBus.regJMXYPart.connect(ctrlIn);
 
-  const dataOut = bgs.z.dataControlBus.dataPart;
+  const addrOut = bgs.y.addressBus.addressPart;
+  const dataOut = bgs.y.dataControlBus.dataPart;
 
   test('ld sel X', function () {
     dataIn.value = BitValue.fromUnsignedNumber(0xdc);
@@ -45,6 +48,17 @@ suite('card-register-xy', () => {
     assert.ok(dataOut.value.isZero);
     ctrlIn.value = BitValue.Zero.flipBit(RegJMXYLines.SEY);
     assert.equal(dataOut.value.toUnsignedNumber(), 0xbc);
+    ctrlIn.value = BitValue.Zero;
+  });
+
+  test('ld sel XY', function () {
+    addrIn.value = BitValue.fromUnsignedNumber(0xabcd);
+    ctrlIn.value = BitValue.Zero.flipBit(RegJMXYLines.LXY);
+    ctrlIn.value = BitValue.Zero;
+    addrIn.value = BitValue.Zero;
+    assert.ok(addrOut.value.isZero);
+    ctrlIn.value = BitValue.Zero.flipBit(RegJMXYLines.SXY);
+    assert.equal(addrOut.value.toUnsignedNumber(), 0xabcd);
     ctrlIn.value = BitValue.Zero;
   });
 
@@ -70,6 +84,52 @@ suite('card-register-xy', () => {
     assert.equal(dataOut.value.toUnsignedNumber(), 0x98);
     ctrlIn.value = BitValue.Zero.flipBit(RegJMXYLines.LDY);
     assert.ok(dataOut.value.isZero);
+  });
+
+  test('ld clr XY', function () {
+    addrIn.value = BitValue.fromUnsignedNumber(0x9876);
+    ctrlIn.value = BitValue.Zero.flipBit(RegJMXYLines.LXY);
+    ctrlIn.value = BitValue.Zero;
+    addrIn.value = BitValue.Zero;
+    assert.ok(addrOut.value.isZero);
+    ctrlIn.value = BitValue.Zero.flipBit(RegJMXYLines.SXY);
+    assert.equal(addrOut.value.toUnsignedNumber(), 0x9876);
+    ctrlIn.value = BitValue.Zero.flipBit(RegJMXYLines.LXY);
+    assert.ok(addrOut.value.isZero);
+  });
+
+  test('ld X Y sel XY', function () {
+    dataIn.value = BitValue.fromUnsignedNumber(0xab);
+    ctrlIn.value = BitValue.Zero.flipBit(RegJMXYLines.LDX);
+    ctrlIn.value = BitValue.Zero;
+    dataIn.value = BitValue.Zero;
+
+    dataIn.value = BitValue.fromUnsignedNumber(0xcd);
+    ctrlIn.value = BitValue.Zero.flipBit(RegJMXYLines.LDY);
+    ctrlIn.value = BitValue.Zero;
+    dataIn.value = BitValue.Zero;
+
+    assert.ok(addrOut.value.isZero);
+    ctrlIn.value = BitValue.Zero.flipBit(RegJMXYLines.SXY);
+    assert.equal(addrOut.value.toUnsignedNumber(), 0xabcd);
+    ctrlIn.value = BitValue.Zero;
+  });
+
+  test('ld XY sel X Y', function () {
+    addrIn.value = BitValue.fromUnsignedNumber(0xfedc);
+    ctrlIn.value = BitValue.Zero.flipBit(RegJMXYLines.LXY);
+    ctrlIn.value = BitValue.Zero;
+    addrIn.value = BitValue.Zero;
+
+    assert.ok(dataOut.value.isZero);
+    ctrlIn.value = BitValue.Zero.flipBit(RegJMXYLines.SEX);
+    assert.equal(dataOut.value.toUnsignedNumber(), 0xfe);
+    ctrlIn.value = BitValue.Zero;
+
+    assert.ok(dataOut.value.isZero);
+    ctrlIn.value = BitValue.Zero.flipBit(RegJMXYLines.SEY);
+    assert.equal(dataOut.value.toUnsignedNumber(), 0xdc);
+    ctrlIn.value = BitValue.Zero;
   });
 
 });
